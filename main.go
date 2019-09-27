807399df27ee57e4725884e7a589ceccd11fbdd0
takuto <takuto.yoshikai@gmail.com>
2019-09-27 11:25:30 +0900

    update main.go

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
   "net/http"
   "viking-game/model"
   "encoding/json"
+  "fmt"
 )
 
-var Accounts []model.Account
+var Accounts []model.Account = MakeSeed()
 
 
 func FindAccount(username string, password string) *model.Account {
   for _, account := range Accounts {
+    fmt.Println(account)
     if account.Username == username && account.Password == password {
       return &account
     }
@@ -20,15 +22,21 @@ func FindAccount(username string, password string) *model.Account {
 
 func ConfigureServer() {
   http.HandleFunc("/user", func (w http.ResponseWriter, r *http.Request) {
-    username := r.URL.Query().Get("username")
     password := r.URL.Query().Get("password")
+    username := r.URL.Query().Get("username")
+    fmt.Println(username)
+    fmt.Println(password)
     account := FindAccount(username, password)
+    w.Header().Set("Content-Type", "application/json")
+    if account == nil {
+      http.Error(w, "Unauthorized", http.StatusUnauthorized)
+      return
+    }
     res, err := json.Marshal(account.Items)
     if err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
     }
-    w.Header().Set("Content-Type", "application/json")
     w.Write(res)
   })
 }
